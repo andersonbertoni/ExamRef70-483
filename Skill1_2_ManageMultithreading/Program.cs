2addb544fc3e4f2da8011a87620b15dad1f2069b
Anderson Cassio Bertoni <andersonbertoni@gmail.com>
2019-04-02 21:42:38 -0300

    + Inclusão dos exemplos 1-47 a 1-50;

--- Skill1_2_ManageMultithreading/Program.cs
+++ Skill1_2_ManageMultithreading/Program.cs
@@ -18,7 +18,11 @@ namespace Skill1_2_ManageMultithreading
             UsingLocking = 3,
             SensibleLocking = 4,
             UsingMonitors = 5,
-            SequentialLocking = 6
+            SequentialLocking = 6,
+            DeadlockedTasks = 7,
+            InterlockTotal = 8,
+            CancelATask = 9,
+            CancelWithException = 10
         }
         static void Main(string[] args)
         {
@@ -82,6 +86,18 @@ namespace Skill1_2_ManageMultithreading
                     case Items.SequentialLocking:
                         SequentialLockingExample();
                         break;
+                    case Items.DeadlockedTasks:
+                        DeadlockedTasksExample();
+                        break;
+                    case Items.InterlockTotal:
+                        InterlockTotalExample();
+                        break;
+                    case Items.CancelATask:
+                        CancelATaskExample();
+                        break;
+                    case Items.CancelWithException:
+                        CancelWithExceptionExample();
+                        break;
                     case Items.Sair:
                         run = false;
                         break;
@@ -93,11 +109,12 @@ namespace Skill1_2_ManageMultithreading
                     Console.ReadKey();
                 }
             }
-        }
+        }       
 
         private static void CleanVariables()
         {
             sharedTotal = 0;
+            cancelationTokenSource = new CancellationTokenSource();
         }
 
         #region Variables
@@ -301,7 +318,7 @@ namespace Skill1_2_ManageMultithreading
 
         #endregion
 
-        #region Sequential Locking Example Methods
+        #region Sequential Locking Example, Deadlocked Tasks Example Methods
 
         static void Method1()
         {
@@ -336,12 +353,143 @@ namespace Skill1_2_ManageMultithreading
             Console.WriteLine("Method 2 released lock2");
         }
 
+        #endregion
+
+        #region Sequential Locking Example Methods
+
         private static void SequentialLockingExample()
         {
             Method1();
             Method2();
         }
 
+        #endregion
+
+        #region Deadlocked Tasks Example Methods
+
+        private static void DeadlockedTasksExample()
+        {
+            Task t1 = Task.Run(() => Method1());
+            Task t2 = Task.Run(() => Method2());
+
+            Console.WriteLine("Waiting for task2");
+            t2.Wait();
+
+            Console.WriteLine("Tasks complete");
+        }
+
+        #endregion
+
+        #region Interlock Total Methods
+
+        static void addRangeOfValuesWithInterlock(int start, int end)
+        {
+            long subtotal = 0;
+
+            while (start < end)
+            {
+                subtotal = subtotal + items[start];
+                start++;
+            }
+
+            Interlocked.Add(ref sharedTotal, subtotal);
+        }
+
+        private static void InterlockTotalExample()
+        {
+            List<Task> tasks = new List<Task>();
+
+            int rangeSize = 1000;
+            int rangeStart = 0;
+
+            while (rangeStart < items.Length)
+            {
+                int rangeEnd = rangeStart + rangeSize;
+
+                if (rangeEnd > items.Length)
+                    rangeEnd = items.Length;
+
+                //create local copies of the parameters
+                int rs = rangeStart;
+                int re = rangeEnd;
+
+                tasks.Add(Task.Run(() => addRangeOfValuesWithInterlock(rs, re)));
+                rangeStart = rangeEnd;
+            }
+
+            Task.WaitAll(tasks.ToArray());
+
+            Console.WriteLine("The total is {0}", sharedTotal);
+        }
+
+        #endregion
+
+        #region Cancel a Task Example Methods and Variable
+
+        static CancellationTokenSource cancelationTokenSource = new CancellationTokenSource();
+
+        static void Clock()
+        {
+            while(!cancelationTokenSource.IsCancellationRequested)
+            {
+                Console.WriteLine("Tick");
+                Thread.Sleep(500);
+            }
+        }
+
+        private static void CancelATaskExample()
+        {
+            Task.Run(() => Clock());
+            Console.WriteLine("Press any key to stop the clock");
+            Console.ReadKey();
+            cancelationTokenSource.Cancel();
+            Console.WriteLine("Clock stopped");
+            Console.ReadKey();
+        }
+
+        #endregion
+
+        #region Cancel with Exception Methods
+
+        static void Clock(CancellationToken cancellationToken)
+        {
+            int tickCount = 0;
+
+            while(!cancellationToken.IsCancellationRequested && tickCount < 20)
+            {
+                tickCount++;
+                Console.WriteLine("Tick");
+                Thread.Sleep(500);
+            }
+
+            cancellationToken.ThrowIfCancellationRequested();
+        }
+
+        private static void CancelWithExceptionExample()
+        {
+            Task clock = Task.Run(() => Clock(cancelationTokenSource.Token));
+
+            Console.WriteLine("Press any key to stop the clock");
+            Console.ReadKey();
+
+            if(clock.IsCompleted)
+            {
+                Console.WriteLine("Clock task completed");
+            }
+            else
+            {
+                try
+                {
+                    cancelationTokenSource.Cancel();
+                    clock.Wait();
+                }
+                catch(AggregateException ex)
+                {
+                    Console.WriteLine("Clock stopped: {0}", ex.InnerExceptions[0].ToString());
+                }
+            }
+        }
+        
         #endregion
     }
 }

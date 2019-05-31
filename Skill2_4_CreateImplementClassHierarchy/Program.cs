ee12f56329ae2c6affab5cabd4ebe2dc8df8d812
andersonbertoni <andersonbertoni@gmail.com>
2019-05-31 17:27:39 -0300

    + Adicionados exemplos 2-36 e 2-37;

--- Skill2_4_CreateImplementClassHierarchy/Program.cs
+++ Skill2_4_CreateImplementClassHierarchy/Program.cs
@@ -15,7 +15,9 @@ namespace Skill2_4_CreateImplementClassHierarchy
             None = 0,
             IPrintableInterface = 1,
             IAccountInterface = 2,
-            BabyAccount = 3
+            BabyAccount = 3,
+            OverridenWithdrawFunds = 4,
+            BaseMethod = 5
         }
 
         static void Main(string[] args)
@@ -48,6 +50,12 @@ namespace Skill2_4_CreateImplementClassHierarchy
                     case Items.BabyAccount:
                         BabyAccountExample();
                         break;
+                    case Items.OverridenWithdrawFunds:
+                        OverridenWithdrawFundsExample();
+                        break;
+                    case Items.BaseMethod:
+                        BaseMethodExample();
+                        break;
                     case Items.Sair:
                         run = false;
                         break;
@@ -60,7 +68,7 @@ namespace Skill2_4_CreateImplementClassHierarchy
                 }
             }
         }
-        
+
         #region Classes and Interfaces
 
         /* Consider a printing service that will print objects in an application. You can create an interface 
@@ -195,6 +203,114 @@ namespace Skill2_4_CreateImplementClassHierarchy
                 return true;
             }
         }        
+
+        /* What you really want to do is pick up all the behaviors in the BankAccount and then just change the
+         * one method that needs to behave differently. This can be done in C# using inheritance. 
+         * The key thing here is the word BankAccount after the class name. This is the name of the class that
+         * BabyAccount2 is extending. This means that everything that BankAccount can do, BabyAccount2 can do.
+         * BankAccount is called the base or parent class of BabyAccount2. */         
+        public class BabyAccount2 : BankAccount, IAccount
+        {
+
+        }
+
+        public class BankAccount2 : IAccount
+        {   
+            /* The WithdrawFunds method in the BabyAccount3 class makes use of the _balance value that
+             * is declared in the parent BankAccount2 class. To make this possible the _balance value
+             * has had its access modifier changed from private to protected so that it can be used in
+             * classes that extend the BankAccount2 class. 
+             * The ability to override a method is very powerful. It means that you can make more general
+             * classes (BankAccount) and customize it to make them more specific (BabyAccount). */
+            protected decimal _balance = 0;
+
+            decimal IAccount.GetBalance()
+            {
+                return _balance;
+            }
+
+            void IAccount.PayInFunds(decimal amount)
+            {
+                _balance = _balance + amount;
+            }
+
+            /* The C# compiler needs to know if a method is going to be overriden. This is because it
+             * must call an overridden method in a slightly different way from a "normal" one. The 
+             * WithdrawFunds method in the BankAccount class has been declared as virtual so that the 
+             * compiler knows it may be overridden. It might be overridden in classes which are children 
+             * of the parent class. 
+             * The C# language does not allow the overriding of explicit implementations of interface 
+             * methods. This means that you have to sacrifice a slight measure of encapsulation in order
+             * to use class hierarchies in this manner. You can see that the WithdrawFunds method is
+             * declared as virtual, but it has not been declared as an interface method. 
+             * You can make the WithdrawFunds method virtual because you discovered that different 
+             * accounts might need to withdraw funds in a different way. Note that the PayInFunds and
+             * GetBalance methods have not been made virtual because you will always be using the 
+             * versions of these declared in the BankAccount2 class. */
+            public virtual bool WithdrawFunds(decimal amount)
+            {
+                if (_balance < amount)
+                    return false;
+
+                _balance = _balance - amount;
+                return true;
+            }
+        }
+
+        /* Overriding replaces a method in a base class with a version that provides the behavior appropriate
+         * to a child class. In the case of the BabyAccount3, you want to change the behavior of the one method
+         * that you are interested in. You want to replace the WithdrawFunds method with a new one. */
+            public class BabyAccount3: BankAccount2, IAccount
+        {
+            /* The keyword override means "use this version of the method in preference to the one in the base 
+             * class". 
+             * The WithdrawFunds method in the BabyAccount3 class contains all the code of the method in the 
+             * parent class. 
+             * We don't like this much, in that it means that the balance value has to be made more exposed
+             * than we might like. It is now protected rather than private to just the BankAccount2 class. */
+            public override bool WithdrawFunds(decimal amount)
+            {
+                if (amount > 10)
+                    return false;
+
+                if (_balance < amount)
+                    return false;
+
+                _balance = _balance - amount;
+
+                return true;
+            }
+        }
+
+        public class BabyAccount4 : BankAccount2, IAccount
+        {            
+            public override bool WithdrawFunds(decimal amount)
+            {
+                if (amount > 10)
+                {
+                    return false;
+                }
+                else
+                {
+                    /* The word base in this context means "a reference to the thing which has been overridden".
+                     * I can use this to make the WithdrawFunds method in my BabyAccount4 much simpler. 
+                     * This line make a call to the original WithdrawFunds method in the parent class. This
+                     * attempts to perform a withdrawal and returns true if the withdrawal works. 
+                     * It's important to understand what we're doing here, and why we're doing it:
+                     *      - We don't want to have to write the same code twice;
+                     *      - We don't want to make the _balance value visible outside the BankAccount class.
+                     * The use of the word base to call the overridden method solves both of these problems 
+                     * rather beautifully. Because the method call returns a bool result you can just send
+                     * whatever it delivers. By making this change you can put the _balance back to private
+                     * in the BankAccount because it is not accessed by the WithdrawFunds method.
+                     * Note that there are other useful spin-offs here. If I need to fix a bug in the behavior
+                     * of the WithdrawFunds method I just fix it once, in the top-level class, and then it 
+                     * is fixed for all the classes which call back to it. */
+                     */
+                    return base.WithdrawFunds(amount);
+                }
+            }
+        }
         
         #endregion
 
@@ -244,6 +360,54 @@ namespace Skill2_4_CreateImplementClassHierarchy
                 Console.WriteLine("Balance: {0}", b.GetBalance());
             else
                 Console.WriteLine("Not allowed");
+
+            /* You can write the code below: */
+            IAccount b2 = new BabyAccount2();
+            b2.PayInFunds(50);
+            Console.WriteLine("Balance: {0}", b.GetBalance());
+
+            /* This works because, although BabyAccount2 does not have a PayInFunds method, the base class does.
+             * This means that the PayInFunds method from the BankAccount class is used at this point.
+             * Instances of the BabyAccount2 class have abilities which they pick up from their base class. In 
+             * fact, at the moment, the BabyAccount2 class has no behaviors of its own; it gets everything from
+             * its base class. */
+        }
+
+        #endregion
+
+        #region OverridenWithdrawFundsExample Method
+
+        private static void OverridenWithdrawFundsExample()
+        {
+            IAccount b = new BabyAccount3();
+            /* When the code runs the call, PayInFunds will use the method in the parent (since that has not 
+             * been overridden), ... */
+            b.PayInFunds(50);
+
+            /* ... but the call of WithdrawFunds will use the method in BabyAccount3. */
+            if (b.WithdrawFunds(10))
+                Console.WriteLine("Withdraw succeeded");
+            else
+                Console.WriteLine("Withdraw failed");
+
+            Console.WriteLine(b.GetBalance());            
+        }
+
+        #endregion
+
+        #region BaseMethodExample Method
+
+        private static void BaseMethodExample()
+        {
+            IAccount b = new BabyAccount4();            
+            b.PayInFunds(50);
+                        
+            if (b.WithdrawFunds(10))
+                Console.WriteLine("Withdraw succeeded");
+            else
+                Console.WriteLine("Withdraw failed");
+
+            Console.WriteLine(b.GetBalance());
         }
 
         #endregion
